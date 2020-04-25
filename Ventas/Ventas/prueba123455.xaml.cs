eb5b9d2c53c21bd022cfa4060f47911b2b1c8fd2
Jonathan3249 <jonathanmanfredo3259@gmail.com>
2020-04-25 15:47:32 -0600

    //prueba11

---
+++ Ventas/Ventas/prueba123455.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ventas
+{
+    /// <summary>
+    /// Lógica de interacción para prueba123455.xaml
+    /// </summary>
+    public partial class prueba123455 : Window
+    {
+        public prueba123455()
+        {
+            InitializeComponent();
+        }
+    }
+}
